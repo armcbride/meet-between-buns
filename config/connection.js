ca343958f7948018c3f661dff322c03183e97d3b
Annaruth McBride <armcbride17@gmail.com>
2020-05-05 12:51:34 -0500

    added jawsDB, adjusted coding to fix handlebar error

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,12 @@ const mysql2 = require("mysql2");
 
 require('dotenv').config();
 
-const connection = mysql2.createConnection({
+const connection = mysql2.createConnection      (process.env.JAWSDB_URL ||{
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "burger_db"
+    database: process.env.DB
 });
 
 connection.connect(function(err) {
